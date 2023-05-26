87c4d978fe0ba4c426bbb5b25cab91c9466bb524
GunjanBisht188 <105977015+GunjanBisht188@users.noreply.github.com>
2023-05-26 21:58:08 +0530

    Add files via upload

---
+++ linkedlist6.java
@@ -0,0 +1,16 @@
+public class linkedlist6 {
+    int key;
+    int value;
+
+ 
+    int timeStamp;
+
+    public Node(int key, int value)
+    {
+        this.key = key;
+        this.value = value;
+
+      
+        this.timeStamp = currentTimeStamp;
+    }
+}
